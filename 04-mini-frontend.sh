961a325291d89c39ec074c059175813466333912
sid2417 <siddhartha.java9398@gmail.com>
2024-05-02 00:13:36 +0530

    Creating DB server

--- 04-mini-frontend.sh
+++ 04-mini-frontend.sh
@@ -9,11 +9,11 @@ N="\e[0m"
 #Date #ScriptName #Logfile
 TIME_STAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo "$?" | cut -d "." -f1)
-LOG_FILE=$SCRIPT_NAME+$TIME_STAMP.log
+LOG_FILE=/tmp/$SCRIPT_NAME+$TIME_STAMP.log
 
 #UserId
 USER_ID=$(id -u)
-if [ USER_ID -ne 0 ]
+if [ $USER_ID -ne 0 ]
 then 
     echo -e "$R Please Provide SUDO access...$N"
     exit 1
@@ -24,7 +24,7 @@ fi
 
 VALIDATE ()
 {
-    if ($1 -ne 0)
+    if [ $1 -ne 0 ]
     then 
         echo -e "$R $2 FAILURE $N"
         exit 1
@@ -34,30 +34,31 @@ VALIDATE ()
 }
 
 
-dnf install nginx -y &>>LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installind nginx : "
 
-systemctl enable nginx &>>LOG_FILE
+systemctl enable nginx &>>$LOG_FILE
 VALIDATE $? "Enabling nginx : "
 
-systemctl start nginx &>>LOG_FILE
+systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting the nginx : "
 
-rm -rf /usr/share/nginx/html/* &>>LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing Default content of nginx : "
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>LOG_FILE
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
 VALIDATE $? "Downloading Frontend application in /tmp folder : "
 
-cd /usr/share/nginx/html &>>LOG_FILE
+cd /usr/share/nginx/html &>>$LOG_FILE
 
-unzip /tmp/frontend.zip &>>LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Unzipping the frontend application : "
 
-vim /etc/nginx/default.d/expense.conf
-cp
+#vim /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/mysql-db/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Congiguration of Frontend to Backend connection is : "
 
-systemctl restart nginx &>>LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting the nginx : "
 
-echo -e "$Y Frontend installation is Going GOOD $N" &>>LOG_FILE
+echo -e "$Y Frontend installation is Going GOOD $N" 
